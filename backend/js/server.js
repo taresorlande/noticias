c128f41f7fe1aac521801bab2b8685b42a87d0e2
Tares Liberato Orlande de Almeida <fac13944@mg.senac.br>
2025-09-12 11:17:52 -0300

    Ajustes de Envirorment

--- backend/js/server.js
+++ backend/js/server.js
@@ -32,7 +32,9 @@ app.delete('/noticia/:id', async (req, res) => {
     }
 });
 
-app.listen(3000, () => {
-    console.log('Servidor rodando em http://localhost:3000');
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
 
